fed68dd45734be89f04eaecde5a8ffc914f6f837
Sam Gardner <samgardner20@gmail.com>
2022-12-09 14:20:48 -0500

    Made the app mostly responsive

---
+++ src/sizes.js
@@ -0,0 +1,17 @@
+import BreakPoints from './styles/main.scss'
+
+const {
+  breakpointMobile,
+  breakpointLandscape,
+  breakpointTablet,
+  breakpointFull } = BreakPoints
+
+
+export default {
+    up(){
+
+    }
+    down(size){
+
+    }
+}
