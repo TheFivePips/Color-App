8d6a4f72cdd1821f956c442ec3448b587b532b8c
Sam Gardner <samgardner20@gmail.com>
2022-12-12 13:25:22 -0500

    changed the app component to a functional component and added in route transistions

--- src/App.js
+++ src/App.js
@@ -1,57 +1,71 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PaletteRouteDetails from './PaletteRouteDetails';
 import { seedColors } from './seedColors';
-import {Route, Routes} from 'react-router-dom'
+import {Route, Routes, useLocation } from 'react-router-dom'
 import PaletteList from './PaletteList';
 // import SingleColorPalette from './SingleColorPalette';
 import SCPRouteDetails from './SCPRouteDetails'
 import NewPaletteForm from './NewPaletteForm';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
-class App extends Component{
-  constructor(props){
-    super(props)
-    const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'))
-    this.state = {
-      palettes: savedPalettes || seedColors
-    }
-    this.savePalette = this.savePalette.bind(this)
-    this.syncLocalStorage = this.syncLocalStorage.bind(this)
-    this.deletePalette = this.deletePalette.bind(this)
-  }
+function App() {
+  
+  const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'))
+    
+  const [palettes, setPalettes] = useState(savedPalettes || seedColors)
+    
+  const location = useLocation()
 
-  savePalette(newPalette){
-    // pass synclocalstorage as a callback to guraentee the palettes get saved in the correct order
-    this.setState({
-      palettes : [...this.state.palettes, newPalette]
-    },this.syncLocalStorage)  
+  const savePalette = (newPalette) => {
+    // pass synclocalstorage as a callback to guraentee the palettes get saved in the correct order 
+    setPalettes([...palettes, newPalette],
+    syncLocalStorage) 
   }
   
-  syncLocalStorage(){
-    window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes))
+  const syncLocalStorage = () => {
+    window.localStorage.setItem('palettes', JSON.stringify(palettes))
   }
 
-  deletePalette(id){
-    this.setState(
-      st => ({palettes: st.palettes.filter(p => p.id !== id)}),
-      this.syncLocalStorage
-    )
+  const deletePalette = (id) => {
+    setPalettes(palettes.filter(p => p.id !== id))
+    syncLocalStorage()
   }
   
-  render() {
-    return (
-      <Routes>
-        <Route path="/" element={<PaletteList palettes={this.state.palettes} deletePalette={this.deletePalette}/>} />
-        <Route
-         path="/palette/:id"
-          element={<PaletteRouteDetails seeds={this.state.palettes}/>}
-        />
-        <Route 
-          path='/palette/:id/:colorId'
-          element={<SCPRouteDetails seeds={this.state.palettes}/>}/>
-        <Route path='/palette/new' element={<NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes}/>}/>
-      </Routes>
-    );
-  }
+  return (
+    <SwitchTransition>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <Routes location={location}>
+          <Route
+            path="/"
+            element={
+              <PaletteList
+                palettes={palettes}
+                deletePalette={deletePalette}
+              />
+            }
+          />
+          <Route
+            path="/palette/:id"
+            element={<PaletteRouteDetails seeds={palettes} />}
+          />
+          <Route
+            path="/palette/:id/:colorId"
+            element={<SCPRouteDetails seeds={palettes} />}
+          />
+          <Route
+            path="/palette/new"
+            element={
+              <NewPaletteForm
+                savePalette={savePalette}
+                palettes={palettes}
+              />
+            }
+          />
+        </Routes>
+      </CSSTransition>
+    </SwitchTransition>
+  );
+  
 }
 
 export default App;
